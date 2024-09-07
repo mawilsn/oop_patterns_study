1f02f200682b21e06b910b30c619c270a8442e4e
Mark Wilson <mawilsn1989@gmail.com>
2024-09-07 17:40:05 -0500

    adding new py stuff

--- py_patterns/factory_method/FactoryMethod.py
+++ py_patterns/factory_method/FactoryMethod.py
@@ -15,6 +15,24 @@ class Creator(ABC):
         Creator may provide some default
         """
         raise NotImplementedError(f"This method is not implemented{factory_method.__name__}")
+    
+    def some_operation(self) -> str:
+        """
+        Creators Primary responsiblity isn't creating products. Usually, contains some core business logic
+        that relieas on product objects, returned by the factory method.
+        Subclasses can indrectly change that buisness loggic by overrideing the factory method
+        and returning a different type of product from it.
+        """
+
+        # Call the factory to create a product object.
+        product = self.factory_method()
+        # Now use the product
+        result = f"creator: the same code has just done {product}"
+        return result
+    
+
+    
+
 
 
 
